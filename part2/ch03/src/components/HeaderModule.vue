264a62ae041a8b3d5d12d7d83692ed8f5710e983
uOung <youyoung0703@gmail.com>
2022-03-30 13:17:38 +0900

    make headerModule

---
+++ part2/ch03/src/components/HeaderModule.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+      <li>
+        <router-link to="/">Main</router-link>
+      </li>
+      <li>
+        <router-link to="/login">Login</router-link>
+      </li>
+      <li>
+        <router-link to="/profile">Profile</router-link>
+      </li>
+    </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+}
+</script>
