adfc7ed0e97d8b01717c9c4daf2692b4b24a1b56
uOung <youyoung0703@gmail.com>
2022-03-30 11:25:04 +0900

    ✨ Main Page vue-router #5

---
+++ part2/ch03/src/pages/MainPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <router-link to="/">Main</router-link>
+      </li>
+      <li>
+        <router-link to="/login">Login</router-link>
+      </li>
+      <li>
+        <router-link to="/profile">Progile</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  }
+}
+</script>
