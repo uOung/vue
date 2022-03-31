9a15a6b1c4b34d10fc54842a4df04fdc1a8b9352
uOung <youyoung0703@gmail.com>
2022-03-31 09:33:26 +0900

    ✨ publishing #11

---
+++ part2/big/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <TheFooter></TheFooter>
+  </div>
+</template>
+
+<script>
+import TheFooter from './components/TheFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TheFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
