7b02e5034334d54d2866d562ac41148e89b83f71
uOung <youyoung0703@gmail.com>
2022-03-31 10:49:02 +0900

    fix: change logo and ment

--- part3/vue3/src/App.vue
+++ part3/vue3/src/App.vue
@@ -2,11 +2,14 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <img alt="Vue logo" src="./assets/photo.png" width="300" height="300" />
+  <HelloWorld msg="Welcome to My Site" />
 </template>
 
 <style>
